5bc134221092023c091835e185f391af3ef22aaf
NareshNakka78 <nareshnakka118@gmail.com>
2023-03-29 08:54:39 +0530

    portfolio created

---
+++ project1/src/pages/Certifications/index.js
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material';
+
+export default function AboutMe(){
+    return(
+        <>
+<Typography> 1.Core java Training by INTERNSHALA TRAININGS</Typography>
+<Typography> 2.Webdevelopment Training by INTERNSHALA TRAININGS</Typography>
+<Typography>  </Typography>
+<Typography> </Typography>
+<Typography> </Typography>
+        </>
+        
+    )
+    }
